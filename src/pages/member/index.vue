1a94813923168e566c27f656e582fe9887761624
chen <cq_shen@126.com>
2022-07-19 19:32:30 +0800

    signUpList

---
+++ src/pages/member/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <view class="member-container">
+    <top title="成员管理" />
+    <search></search>
+
+    <view class="member-wrap">
+      <scroll-view  scroll-y="true"
+          show-scrollbar="true">
+        <view class="list">
+          <view class="list-title">
+            <view class="left"><text class="text">成员列表</text></view>
+            <image
+              class="image"
+              src="//mall-lyxcx.oss-cn-hangzhou.aliyuncs.com/front_end/icon/icon_edit.png"
+            />
+          </view>
+
+          <view class="list-content">
+            <view class="item item-title">
+              <text>姓名</text>
+              <text>职位</text>
+              <text>积分</text>
+            </view>
+            <view
+              v-for="(item, index) in list"
+              :key="index"
+              class="item item-con"
+            >
+              <text class="left">{{ item.name }}</text>
+              <text>{{ item.position }}</text>
+              <text>{{ item.point }}</text>
+            </view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
+  </view>
+</template>
+
+<script>
+import Top from '@/components/top/Top';
+import Search from '@/components/pageSearch/PageSearch';
+export default {
+  components: {
+    Top,
+    Search,
+  },
+  data() {
+    return {
+      list: [
+        {
+          name: '广合',
+          position: '队长',
+          point: '2410',
+        },
+        {
+          name: '广合',
+          position: '队长',
+          point: '2410',
+        },
+        {
+          name: '广合',
+          position: '队长',
+          point: '2410',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+/deep/ .top {
+  padding: 70px 30px 180px 30px !important;
+  .back-icon {
+    top: 70px !important;
+  }
+}
+
+/deep/ .page-search {
+  position: fixed !important;
+  top: 96px;
+  left: 30px;
+  z-index: 100;
+}
+.member-container {
+}
+.member-wrap {
+  position: fixed;
+  left: 30px;
+  right: 30px;
+  top: 240px;
+  bottom: 50px;
+  z-index: 100;
+  padding: 30px;
+  background: linear-gradient(335deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%);
+  box-shadow: 0px 17px 23px 0px rgba(138, 131, 168, 0.1);
+  border-radius: 20px;
+  .scroll-view {
+    height: 100%;
+  }
+}
+
+.list-title,
+.item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: 26px;
+  text-align: center;
+}
+.list-title {
+    margin-bottom: 25px;
+  .left {
+    background-color: rgba(23, 170, 125, 0.1);
+    border-radius: 30px;
+    height: 60px;
+    width: 276px;
+    position: relative;
+    .text {
+      position: absolute;
+      left: 0;
+      width: 176px;
+      height: 60px;
+      background: #17aa7d;
+      border-radius: 30px 0 0 30px;
+      line-height: 60px;
+      color: #fff;
+      text-align: center;
+    }
+  }
+  .image {
+    width: 36px;
+    height: 36px;
+  }
+}
+.item {
+  margin-bottom: 25px;
+}
+.item-title {
+  color: #929292;
+}
+.item-con {
+  color: #17aa7d;
+
+  .left {
+    color: #000;
+  }
+}
+</style>
---
+++ src/pages/signUpList/index.vue
@@ -0,0 +1,163 @@
+<template>
+  <view class="list-container">
+    <top title="报名列表" />
+
+    <view class="list">
+      <view class="list-item list-item-title">
+        <view class="left">排名</view>
+        <view class="center">姓名</view>
+        <view class="right">付款状态</view>
+      </view>
+
+      <scroll-view scroll-y="true" show-scrollbar="true" class="scroll-view">
+        <view class="list-item" v-for="(item, index) in list" :key="index">
+          <view class="left" v-if="index <= 2">
+            <image class="image" :src="topImageUrl[index]" />
+          </view>
+
+          <view class="left" v-if="index > 2"
+            ><text class="text">{{ index }}</text></view
+          >
+          <view class="center"
+            ><text>{{ item.name }}</text></view
+          >
+          <view
+            :class="[
+              'right',
+              {
+                paid: item.status == 1,
+                unpaid: item.status == 2,
+                pay_failed: item.status == 3,
+              },
+            ]"
+          >
+            <text>{{ getStatusText(item.status) }}</text>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
+  </view>
+</template>
+<script>
+import Top from '@/components/top/Top';
+export default {
+  components: {
+    Top,
+  },
+  data() {
+    return {
+      list: [
+        //0，取消支付；1，已支付；2，未支付；3，支付失败
+        {
+          name: '广河',
+          status: 0,
+        },
+        {
+          name: '广河',
+          status: 1,
+        },
+        {
+          name: '广河',
+          status: 1,
+        },
+        {
+          name: '广河',
+          status: 2,
+        },
+        {
+          name: '广河',
+          status: 3,
+        },
+      ],
+      topImageUrl: [
+        '//mall-lyxcx.oss-cn-hangzhou.aliyuncs.com/front_end/list_top1.png',
+        '//mall-lyxcx.oss-cn-hangzhou.aliyuncs.com/front_end/list_top2.png',
+        '//mall-lyxcx.oss-cn-hangzhou.aliyuncs.com/front_end/list_top3.png',
+      ],
+    };
+  },
+  methods: {
+    getStatusText(status) {
+      switch (status) {
+        case 0:
+          return '取消支付';
+          break;
+        case 1:
+          return '已支付';
+          break;
+        case 2:
+          return '未支付';
+          break;
+        case 3:
+          return '支付失败';
+          break;
+        default:
+          return '取消支付';
+          break;
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scope>
+/deep/ .top {
+  padding: 70px 30px 60px 30px !important;
+  border-radius: 0px 0px 0px 50px;
+  .back-icon {
+    top: 70px !important;
+  }
+}
+
+.list {
+  position: fixed;
+  z-index: 100;
+  left: 30px;
+  right: 30px;
+  top: 155px;
+  bottom: 50px;
+  padding: 40px 30px;
+  background: linear-gradient(335deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%);
+  box-shadow: 0px 17px 23px 0px rgba(138, 131, 168, 0.1);
+  border-radius: 20px;
+  .scroll-view {
+    height: 100%;
+  }
+  .list-item {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: 26px;
+    color: #000;
+    margin-bottom: 25px;
+    text-align: center;
+    .center {
+      flex: 1;
+    }
+    .right {
+      width: 120px;
+      text-align: left;
+      color: #b1b1b1;
+      &.paid {
+        color: #17aa7d;
+      }
+      &.unpaid {
+        color: #f8ab52;
+      }
+      &.pay_failed {
+        color: #aa4717;
+      }
+    }
+    .text {
+      display: inline-block;
+      width: 33px;
+    }
+    .image {
+      width: 33px;
+      height: 43px;
+    }
+  }
+  .list-item-title {
+    color: #929292;
+  }
+}
+</style>
